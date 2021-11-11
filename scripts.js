2247b447a984a4d7be4d3ffbad48bbafca49b914
Julian Robbert <julian.robbert@gmail.com>
2021-11-10 16:09:38 -0800

    added functions playround and game, finished project.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,67 @@
-/*this functions defines the variable throwChoice which is assigned a
-value between 1 and 3. these values coincide with either rock paper
-or scissors which the function returns with the switch statement*/
+/*
+define variable that is either 1, 2 or 3
+return rock if 1
+return paper if 2
+return scissors if 3
+*/
 function computerPlay(){
     let throwChoice = Math.floor(Math.random()*3) + 1;
     switch (throwChoice){
         case 1:
-            return 'Rock';
+            return 'rock';
         case 2:
-            return 'Paper';
+            return 'paper';
         case 3:
-            return 'Scissors';
+            return 'scissors';
     }
 }
-console.log(computerPlay());
+
+//keep track of how many times user and comp has won
+let playerWinCount = 0;
+let computerWinCount = 0;
+
+//prompt user for rock paper or scissors
+const playerSelection = prompt("Enter rock paper or scissors!");
+
+/*
+generate a new computer selection at the start of each round
+return who won each round
+increase counter based on who won
+*/
+function playRound(computerSelection, playerSelection){
+    computerSelection = computerPlay();
+    switch (true){
+        case computerSelection === playerSelection:
+            return 'you drew round ';
+        case 
+        (computerSelection === 'rock' && playerSelection === 'scissors') ||
+        (computerSelection === 'paper' && playerSelection === 'rock') ||
+        (computerSelection === 'scissors' && playerSelection === 'paper'):
+            computerWinCount++;
+            return 'you lost round ';
+        case
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper'):
+            playerWinCount++;
+            return 'you won round ';
+    }
+}
+
+/*
+call and print playRound 5 times
+compare the counters and see who won the overall game
+*/
+function game(){
+    for (let i = 1; i <= 5; i++){
+        console.log(playRound(computerPlay(), playerSelection.toLowerCase()) + i);
+    }
+    if (playerWinCount > computerWinCount){
+        return 'that means you won the game!';
+    }else if (playerWinCount < computerWinCount){
+        return 'that means you lost the game!';
+    }else{
+        return 'that means the game was a draw!';
+    }
+}
+console.log(game());
