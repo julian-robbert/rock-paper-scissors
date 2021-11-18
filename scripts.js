217de09a88f674d99f0cdf2cdaddee16512760a4
Julian Robbert <julian.robbert@gmail.com>
2021-11-18 15:08:50 -0800

    added a pic for tie, some more fumbling with scripts

--- scripts.js
+++ scripts.js
@@ -21,13 +21,17 @@ let playerWinCount = 0;
 let computerWinCount = 0;
 
 //prompt user for rock paper or scissors
-//const playerSelection = prompt("Enter rock paper or scissors!");
+const playerSelection = prompt("Enter rock paper or scissors!");
 
 /*
 generate a new computer selection at the start of each round
 return who won each round
 increase counter based on who won
+appends win text into results paragraph
 */
+const computerWinCounterDisplay = document.querySelector('#computerWinCounterDisplay');
+const playerWinCounterDisplay = document.querySelector('#playerWinCounterDisplay');
+
 function playRound(computerSelection, playerSelection){
     computerSelection = computerPlay();
     switch (true){
@@ -38,47 +42,55 @@ function playRound(computerSelection, playerSelection){
         (computerSelection === 'paper' && playerSelection === 'rock') ||
         (computerSelection === 'scissors' && playerSelection === 'paper'):
             computerWinCount++;
+            computerWinCounterDisplay.append(computerWinCount);
             return 'you lost round ';
         case
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper'):
             playerWinCount++;
+            playerWinCounterDisplay.append(playerWinCount);
             return 'you won round ';
     }
 }
+playRound(computerSelection, playerSelection);
 
 /*
-call and print playRound 5 times
+call and print playRound 
 compare the counters and see who won the overall game
 */
-function game(){
-    for (let i = 1; i <= 5; i++){
-        console.log(playRound(computerPlay(), playerSelection.toLowerCase()) + i);
-    }
+const resultsContainer = document.querySelector('#resultsContainer');
+const results = document.createElement('div');
+results.classList.add('results');
+results.style.cssText = 'display: flex; justify-content: center; align-items: center; padding: 20px';
+function whoWon(){
     if (playerWinCount > computerWinCount){
-        return 'You won the game!';
+        results.innerHTML = '<h1>You won the game!</h1>';
     }else if (playerWinCount < computerWinCount){
-        return 'You lost the game!';
+        results.innerHTML = '<h1>You lost the game!</h1>';
     }else{
-        return 'The game was a draw!';
+        results.innerHTML = '<h1>The game was a draw!</h1>';
     }
 }
+whoWon();
+resultsContainer.appendChild(results);
+
+/*//creates and appends the message at the bottom depending on who won
 const resultsContainer = document.querySelector('#resultsContainer');
 const results = document.createElement('div');
 results.style.cssText = 'display: flex; justify-content: center; align-items: center; padding: 20px';
-results.innerHTML = '<h1>You won the game!</h1>';
-results.classList.add('results');
-resultsContainer.appendChild(results);
-
-
-//game();
-/*if (game() === 'You won the game!'){
-    results.innterHTML = '<h1>You won the game!</h1>';
+game();
+if (game() === 'You won the game!'){
+    results.innerHTML = '<h1>You won the game!</h1>';
 }else if (game() === 'You lost the game!'){
     results.innerHTML = '<h1>You lost the game!</h1>';
 }else{
     results.innerHTML = '<h1>The game was a draw!</h1>';
-}*/
+}
+results.classList.add('results');
+resultsContainer.appendChild(results);*/
+
+
+
 
 
